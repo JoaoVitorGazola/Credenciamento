ed4c8a862531f348bc3cac53145b0a164d56bbcb
Luiz Marcelo Freire <43960599+LuizMarceloFreire@users.noreply.github.com>
2019-01-23 12:51:20 -0200

    commit final 23/01/2019

--- sincred/resources/views/farmacia/farmacia.blade.php
+++ sincred/resources/views/farmacia/farmacia.blade.php
@@ -6,7 +6,8 @@
         <div class="col-md-12 col-sm-12 col-lg-12">
             <div class="card">
                 <div class="card-header">
-                 <h2>Farmácias </h2></div>
+                 <h2> Farmácias </h2>
+             </div>
 
                 <div class="card-body">
                     @if (session('encontrado'))
