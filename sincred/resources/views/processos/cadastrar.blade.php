bdf16b2c96674b456026631ef47b29ce62b7ef00
Joao Vitor Gazola <joao.v.gazola@gmail.com>
2019-01-28 12:54:05 -0300

    Revert "Revert "Fim dia 28/01""
    This reverts commit 87f1a5cc9d6537911a46b7036410c9afca42822d.

--- sincred/resources/views/processos/cadastrar.blade.php
+++ sincred/resources/views/processos/cadastrar.blade.php
@@ -1,120 +1,116 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-sm-12 col-lg-12">
-            <div class="card">
-                <div class="card-header"> <h2> Cadastrar Processo </h2>
-                	<br>
-                   
-                    <div class="panel with-nav-tabs panel-primary">
-                        <div class="panel-heading">
-                            <ul class="nav nav-tabs" style="margin-bottom: -13px;">
-                                <li class="active nav-link">
-                            
-                                    <a href="{{url('processos/novo')}}" style="text-decoration: none; color: #212529;"> 
-                                        <i class="fas fa-copy"></i>
-                                    Processo </a>
-                            
-                                </li>
-                                <li class="disabled nav-link">
-                                
-                                    <i class="fas fa-file"></i>
-                                  Documento Requisitado   
-                                    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-sm-12 col-lg-12">
+                <div class="card">
+                    <div class="card-header"> <h2> Cadastrar Processo </h2>
+                        <br>
 
-                                
-                                </li>
-                                <li class="disabled nav-link">
-                                
-                                    <i class="fas fa-file-alt"></i>
+                        <div class="panel with-nav-tabs panel-primary">
+                            <div class="panel-heading">
+                                <ul class="nav nav-tabs" style="margin-bottom: -13px;">
+                                    <li class="active nav-link">
+
+                                        <a href="{{url('processos/novo')}}" style="text-decoration: none; color: #212529;"> <i class="fas fa-copy"></i>Processo </a>
+
+                                    </li>
+                                    <li class="disabled nav-link">
+                                        <i class="fas fa-file"></i>
+
+                                        Documento Requisitado
+
+
+
+                                    </li>
+                                    <li class="disabled nav-link">
+                                        <i class="fas fa-file-alt"></i>
+
                                         Palavras
-                                
-                               
-                                </li>
-                          
-                            </ul>
+
+
+                                    </li>
+
+                                </ul>
+                            </div>
+
                         </div>
-            
-                     </div>
 
-                </div>
+                    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="alert alert-primary" role="alert">
+                            Os campos marcados com * são de preenchimento obrigatório.
                         </div>
-                    @endif
+                        <br>
 
-                    <div class="alert alert-primary" role="alert">
-					  Os campos marcados com * são de preenchimento obrigatório.
-					</div>
-                    <br>
 
-                   
 
-                    <h2> Informações do Processo </h2>
+                        <h2> Informações do Processo </h2>
 
-                    <li style="border-top: 2px #efefef solid; margin-top: 0px; margin-bottom: 0px; display: block;"> </li>
-                    <br>
+                        <li style="border-top: 2px #efefef solid; margin-top: 0px; margin-bottom: 0px; display: block;"> </li>
+                        <br>
 
-                     
-                        {!!Form::open(['url'=>'/'])!!}
+
+                        {!!Form::open(['url'=>'/processos/novo/salvar'])!!}
                         <div class="row">
-                            
+
                             <div class="col-lg-6 col-sm-6 col-md-6">
-                            <strong>{!! Form::label('nome', 'Nome *') !!}</strong>
-                            {!! Form::input('text', 'nome', null, ['class' => 'form-control', 'autofocus', 'placeholder' => 'Nome do Processo']) !!}
-                            </div>
-                            
-                            <div class="col-sm-3 col-lg-3 col-md-3">
-                            <strong>{!! Form::label('inicio', 'Data Início *') !!}</strong>
-
-                            {!! Form::date('inicio', null, ['class' => 'form-control', 'autofocus', 'placeholder' => '00/00/0000']) !!}<small>Data de Início do Processo.</small>
+                                {!! Form::label('nome', 'Nome *') !!}
+                                {!! Form::input('text', 'nome', null, ['class' => 'form-control', 'autofocus', 'placeholder' => 'Nome do Processo']) !!}
                             </div>
 
                             <div class="col-sm-3 col-lg-3 col-md-3">
-                            <strong> {!! Form::label('final', 'Data Final *') !!} </strong>
-                            {!! Form::date('final', null, ['class' => 'form-control', 'autofocus', 'placeholder' => '00/00/0000']) !!}<small>Data de Final do Processo.</small>
+                                {!! Form::label('inicio', 'Data Início *') !!}
+
+                                {!! Form::date('inicio', null, ['class' => 'form-control', 'autofocus', 'placeholder' => '00/00/0000']) !!}
+                            </div>
+
+                            <div class="col-sm-3 col-lg-3 col-md-3">
+                                {!! Form::label('final', 'Data Final *') !!}
+                                {!! Form::date('final', null, ['class' => 'form-control', 'autofocus', 'placeholder' => '00/00/0000']) !!}
 
 
                             </div>
 
                             <div class="col-sm-12 col-lg-12 col-md-12"> <br>
-                            <strong>   {!! Form::label('desc', 'Descrição') !!} </strong>
-                                {!! Form::textarea('textarea', null, ['class' => 'form-control', 'placeholder' => 'Descrição', 'rows' =>'5']) !!}
-                           
+                                {!! Form::label('descrição', 'Descrição') !!}
+                                {!! Form::textarea('descrição', null, ['class' => 'form-control', 'placeholder' => 'Descrição', 'rows' =>'5']) !!}
+
 
                             </div>
 
-                           
 
-                            {!! Form::close() !!}
-                     </div>
-                    <br>
-    
-                    <br>
+                        </div>
+                        <br>
 
-                     <li style="border-top: 2px #efefef solid; margin-top: 0px; margin-bottom: 0px; display: block;"> </li>
+                        <br>
 
-                     <br>
-					
-					
-					<br>	
-						<div class="float-right">
+                        <li style="border-top: 2px #efefef solid; margin-top: 0px; margin-bottom: 0px; display: block;"> </li>
+
+                        <br>
+
+
+                        <br>
+                        <div class="float-right">
                             <button class="btn btn-primary"><a href="/processos" style="color: #fff; text-decoration: none;">Cancelar</a></button>
-							<button class="btn btn-primary"><a href="{{url('documentos/novo')}}" style="color: #fff; text-decoration: none;">Cadastrar</a></button>
+                            {!! Form::submit('Cadastrar', ['class'=>'btn btn-primary']) !!}
+                            {!! Form::close() !!}
 
 
 
-							
-						</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
